95be48f0d66f01f531525b4070f2ac6d5e5c90e5
seyithan3457 <seyithan3457@gmail.com>
2026-06-29 17:56:33 +0300

    Spring Boot projesi oluşturuldu.

---
+++ src/main/java/com/example/gallerist/GalleristApplication.java
@@ -0,0 +1,13 @@
+package com.example.gallerist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GalleristApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GalleristApplication.class, args);
+	}
+
+}
